b3b5d4a6d1d982603eed0e2938727da36b60045d
SnezanaL <snezanalalatovic@gmail.com>
2023-12-18 09:06:15 +0100

    Added components, navigation and footer

---
+++ src/components/Footer.js
@@ -0,0 +1,41 @@
+import img from './../images/Mario and Adrian A.jpg';
+
+const Footer = () => {
+  return (
+    <div className="footer container">
+      <div className="col image">
+        <img src={img}/>
+      </div>
+      <div className="col links">
+        <h3>Doormat Navigation</h3>
+        <ul>
+          <li><a href="">Home</a></li>
+          <li><a href="">About</a></li>
+          <li><a href="">Menu</a></li>
+          <li><a href="">Reservations</a></li>
+          <li><a href="">Order Online</a></li>
+          <li><a href="">Login</a></li>
+        </ul>
+      </div>
+      <div className="col links">
+      <h3>Doormat Navigation</h3>
+        <ul>
+          <li><a href="">Address</a></li>
+          <li><a href="">003 300 44</a></li>
+          <li><a href="">email@gmail.com</a></li>
+        </ul>
+      </div>
+      <div className="col links">
+      <h3>Doormat Navigation</h3>
+        <ul>
+          <li><a href="">Facebook</a></li>
+          <li><a href="">Facebook</a></li>
+          <li><a href="">Facebook</a></li>
+          <li><a href="">Facebook</a></li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import Nav from "./Nav";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="container">
+        <Nav />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
